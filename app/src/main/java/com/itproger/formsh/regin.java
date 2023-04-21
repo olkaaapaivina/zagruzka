bafb5bfa96415d77536bf6539f56ca8128917ffc
mobile <mail>
2023-04-21 13:41:12 +0500

    first commit

---
+++ app/src/main/java/com/itproger/formsh/regin.java
@@ -0,0 +1,14 @@
+package com.itproger.formsh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class regin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regin);
+    }
+}
